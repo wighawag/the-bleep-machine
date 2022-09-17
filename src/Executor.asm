609734fc957a1d3685fdc8c1b1410b05e64535d3
wighawag <wighawag@gmail.com>
2022-09-17 10:13:22 +0100

    Skeleton executor

---
+++ src/Executor.asm
@@ -0,0 +1,16 @@
+codecopy(0, $contract, #contract)
+; Return the contract
+return(0, #contract)
+
+@contract {
+
+
+  // TODO
+  @loop:
+    ; ------------------------------
+    JUMPDEST
+
+    JUMPDEST
+    ; ------------------------------
+
+}
