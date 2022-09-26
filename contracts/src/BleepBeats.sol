14a1b2e289922e2399b7c3d7b7bdc024101e27ef
wighawag <wighawag@gmail.com>
2022-09-26 18:55:14 +0100

    id shifted for display too

--- contracts/src/BleepBeats.sol
+++ contracts/src/BleepBeats.sol
@@ -84,9 +84,10 @@ contract BleepBeats is
 		unchecked {
 			shift = (Utils.numLeadingZeroes(id) / 8) * 8;
 		}
+        id = id << shift;
 		bytes memory musicBytecode = new bytes((256 - shift) / 8);
 		assembly {
-			mstore(add(musicBytecode, 32), shl(shift, id))
+			mstore(add(musicBytecode, 32), id)
 		}
 
 		str = string(
