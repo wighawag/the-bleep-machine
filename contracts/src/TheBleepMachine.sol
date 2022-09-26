0a3fe4fcd472d0de69487626995dbcae5876f216
wighawag <wighawag@gmail.com>
2022-09-26 15:32:38 +0100

    generic Bleep Machine

--- contracts/src/TheBleepMachine.sol
+++ contracts/src/TheBleepMachine.sol
@@ -2,12 +2,12 @@
 pragma solidity 0.8.16;
 
 import "base64-sol/base64.sol";
-import "hardhat-deploy/solc_0.8/proxy/Proxied.sol";
 
-contract TheBleepMachine is Proxied{
+error MusicByteCodeTooLarge();
+error MusicContractCreationFailure();
+error MusicExecutionFailure();
 
-    constructor() {}
-    function postUpgrade() external proxied {}
+contract TheBleepMachine{
 
 	function play(
 		bytes memory musicBytecode,
@@ -42,61 +42,38 @@ contract TheBleepMachine is Proxied{
 		uint256 length
 	) internal returns (bytes memory) {
 		bytes memory executorCreation = bytes.concat(
-            hex"61006d600e60003961006d6000f36000358060801b806000529060801c60205260006040525b",
+            hex"61006d600081600b8239f36000358060801b806000529060801c60205260006040525b",
 			musicBytecode,
 			hex"60ff9016604051806080019091905360010180604052602051600051600101806000529110601757602051806060526020016060f3"
 		);
         uint256 len = musicBytecode.length;
 
 
-        uint256 codeLen = 0x4d + len;
-        require(codeLen <= 0xFFFF, "TOO_LONG");
+        uint256 codeLen;
+        unchecked {
+            codeLen = 0x4d + len;
+        }
+        if( codeLen > 0xFFFF) {
+            revert MusicByteCodeTooLarge();
+        }
         assembly {
+            mstore8(add(executorCreation, 33), shr(8, codeLen))
 			mstore8(add(executorCreation, 34), and(codeLen, 0xFF))
-			mstore8(add(executorCreation, 33), and(shr(8, codeLen), 0xFF))
-
-			mstore8(add(executorCreation, 42), and(codeLen, 0xFF))
-			mstore8(add(executorCreation, 41), and(shr(8, codeLen), 0xFF))
 		}
-
-
-        // ------------------------------------------------------------------------------------------------------------
-        // OLD IMPL
-        // ------------------------------------------------------------------------------------------------------------
-        // bytes
-        // memory executorCreation = hex"606d600c600039606d6000f36000358060801b806000529060801c60205260006040525b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b60ff9016604051806080019091905360010180604052602051600051600101806000529110601757602051806060526020016060f3";
-
-
-		// uint256 len = musicBytecode.length;
-		// uint256 mask = 256**(32 - len) - 1;
-		// assembly {
-		// 	let src := add(musicBytecode, 32)
-		// 	let dest := add(executorCreation, 68) // 32 + 36 where JUMPSET start (second one)
-		// 	for {
-
-		// 	} gt(len, 31) {
-		// 		len := sub(len, 32)
-		// 		dest := add(dest, 32)
-		// 		src := add(src, 32)
-		// 	} {
-		// 		mstore(dest, mload(src))
-		// 	}
-
-		// 	let srcpart := and(mload(src), not(mask)) // NOTE can remove that step by ensuring the length is a multiple of 32 bytes
-		// 	let destpart := and(mload(dest), mask)
-		// 	mstore(dest, or(destpart, srcpart))
-		// }
-        // ------------------------------------------------------------------------------------------------------------
-
 
 		address executor;
 		assembly {
 			executor := create(0, add(executorCreation, 32), mload(executorCreation))
 		}
-		require(executor != address(0), "CREATE_FAILS");
+
+		if(executor == address(0)) {
+            revert MusicContractCreationFailure();
+        }
 
 		(bool success, bytes memory buffer) = executor.staticcall(abi.encode(start | (length << 128)));
-		require(success, "CALL_FAILS");
+		if (!success) {
+            revert MusicExecutionFailure();
+        }
 
 		return buffer;
 	}
