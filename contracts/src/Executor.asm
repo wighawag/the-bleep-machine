e1bed853ddf893e5beddd10227e4f52caca3d64d
wighawag <wighawag@gmail.com>
2022-09-29 18:02:17 +0100

    doc for Executor

--- contracts/src/Executor.asm
+++ contracts/src/Executor.asm
@@ -6,16 +6,16 @@ return(0, #contract)
 ; // LENGTH =  calldata first 16 bytes
 ; // OFFSET = in buffer
 @contract {
-  calldataload(0)  ; [calldata]
-  dup1()          ; [calldata, calldata]
-  shl(128, $$)  ; [calldata, START << 128]
-  shr(128, $$)  ; [calldata, START]
-  dup1()        ; [calldata, START, START]
+  calldataload(0)   ; [calldata]
+  dup1()            ; [calldata, calldata]
+  shl(128, $$)      ; [calldata, START << 128]
+  shr(128, $$)      ; [calldata, START]
+  dup1()            ; [calldata, START, START]
 
-  mstore(0, $$) ; [calldata, START,]  // START stored at 0
-  swap1()       ; [START, calldata]
-  shr(128, $$)  ; [START, LENGTH]
-  mstore(32, $$) ; [START] // LENGTH stored at 32
+  mstore(0, $$)     ; [calldata, START]  // START stored at 0
+  swap1()           ; [START, calldata]
+  shr(128, $$)      ; [START, LENGTH]
+  mstore(32, $$)    ; [START] // LENGTH stored at 32
 
   @loop:
     ; ------------------------------
@@ -28,16 +28,17 @@ return(0, #contract)
     dup2()          ; [OFFSET, 0x05, OFFSET]
     add(96, $$)     ; [OFFSET, 0x05, BUFFER_INDEX]
 		mstore8($$, $$) ; [OFFSET]
-    add(1, $$)      ; [OFFSET] // + 1
-    dup1()          ; [OFFSET, OFFSET]
-    mstore(64, $$)  ; [OFFSET]
-    mload(0)        ; [OFFSET, START]
-		dup2()          ; [OFFSET, START, OFFSET]
-		add($$,$$)      ; [OFFSET, TIME]
-		swap1()         ; [TIME, OFFSET]
-		mload(32)				; [TIME, OFFSET, LENGTH]
-  jumpi($loop, gt($$, $$)) ; [TIME]
+    add(1, $$)      ; [OFFSET+1]
+    dup1()          ; [OFFSET+1, OFFSET+1]
+    mstore(64, $$)  ; [OFFSET+1]
+    mload(0)        ; [OFFSET+1, START]
+		dup2()          ; [OFFSET+1, START, OFFSET+1]
+		add($$,$$)      ; [OFFSET+1, TIME]
+		swap1()         ; [TIME, OFFSET+1]
+		mload(32)				; [TIME, OFFSET+1, LENGTH]
+		gt($$, $$)      ; [TIME, LENGTH > OFFSET+1]
+  jumpi($loop, $$)  ; [TIME]
 
   mload(32)         ; [TIME, LENGTH]  (get the length from memory)
-  return (96, $$)  ;
+  return (96, $$)   ;
 }
